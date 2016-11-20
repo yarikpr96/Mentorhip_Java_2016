6a8487299dd3ed9da6e36db5462e39cd4c5fa391
yarikpr96 <yaroslav0396@ukr.net>
2016-11-20 15:14:45 +0200

    створив інтерфейс для пошуку квітів

---
+++ src/main/java/Home_1_FlowerShop/Interface/FindFlower.java
@@ -0,0 +1,13 @@
+package Home_1_FlowerShop.Interface;
+
+import Home_1_FlowerShop.Flower;
+
+
+public interface FindFlower {
+
+    void findObject(Flower flower);
+
+    void findLongestFlower(String nameBouquet);
+
+    void findShortestFlower(String nameBouquet);
+}
