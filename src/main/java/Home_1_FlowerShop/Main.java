8e1d306ba8f49ea3bbdf5f1867891e4ff476aa48
yarikpr96 <yaroslav0396@ukr.net>
2016-11-20 15:21:57 +0200

    Створив мейн

---
+++ src/main/java/Home_1_FlowerShop/Main.java
@@ -0,0 +1,19 @@
+package Home_1_FlowerShop;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Shop shop = new Shop();
+        shop.newBouquetRand("Bouquet", 10);
+        Flower flower = new Flower();
+        flower.setColorFlower("White");
+        System.out.println("*******************************");
+        shop.findObject(flower);
+        System.out.println("*******************************");
+        shop.findLongestFlower("Bouquet");
+        System.out.println("*******************************");
+        shop.findShortestFlower("Bouquet");
+        System.out.println("*******************************");
+        shop.allBouquet();
+    }
+}
