f922a08d3ae640b1b1296a4cefecd29cc3b948df
yarikpr96 <yaroslav0396@ukr.net>
2016-11-25 19:29:08 +0200

    v2.0

---
+++ src/main/java/SOLID/with/Example1/OrderRepositoryImpl.java
@@ -0,0 +1,19 @@
+package SOLID.with.Example1;
+
+
+public class OrderRepositoryImpl  {
+
+    public void saveOrder() {
+
+    }
+
+
+    public void updateOrder() {
+
+    }
+
+
+    public void deleteOrder() {
+
+    }
+}
---
+++ src/main/java/SOLID/with/Example4/DAOCustomer.java
@@ -0,0 +1,23 @@
+package SOLID.with.Example4;
+
+import SOLID.with.Iinterface.AddCustomerToData;
+
+/**
+ * Created by Ярослав on 25.11.2016.
+ */
+public class DAOCustomer implements AddCustomerToData {
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
